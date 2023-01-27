af6c6c7e4d429508ef67437fd7ee1fc12603972b
kchahda <kchahda@uci.edu>
2023-01-27 15:42:21 -0800

    this is a commit #1

--- update-version.sh
+++ update-version.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-git status 
+git status
+
+touch version
 
 current=$(date)
 
-echo "$current"
+echo "$current" > version
 
+git add version update-version.sh
 
+git commit -m "this is a commit #1"
+
+git push 
